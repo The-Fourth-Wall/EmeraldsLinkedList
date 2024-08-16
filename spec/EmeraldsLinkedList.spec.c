a5b8b0e4ec25ee501ba177b5b8510476be87cd40
oblivious <thanos.papap42@gmail.com>
2024-08-16 12:49:59 +0300

    Migrated to new cSpec.

--- spec/EmeraldsLinkedList.spec.c
+++ spec/EmeraldsLinkedList.spec.c
@@ -2,10 +2,10 @@
 #include "linked_list_dup/linked_list_dup.module.spec.h"
 #include "linked_list_functional_functions/linked_list_functional_functions.module.spec.h"
 
-spec_suite({
-  T_linked_list_base();
-  T_linked_list_dup();
-  T_linked_list_functional_functions();
-});
-
-int main(void) { run_spec_suite("all"); }
+int main(void) {
+  cspec_run_suite("all", {
+    T_linked_list_base();
+    T_linked_list_dup();
+    T_linked_list_functional_functions();
+  });
+}
